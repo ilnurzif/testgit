85502b4c986126dc10647fad02068c21b580ea0a
ilnur Zagyrtdinov <IfZagyrtdinov@sberbank.ru>
2023-01-22 11:39:42 +0300

    6 commit

--- app/src/main/java/com/example/myapplication/wer.kt
+++ app/src/main/java/com/example/myapplication/wer.kt
@@ -1,12 +1,5 @@
 package com.example.myapplication
 
 class wer {
-    val er = 456
-    val k = "vefvdfvf"
-    val r = 3456
-    val g = 6
-    val d = 9
-    val f = "sdsdvsd"
-    val e = 678
-    val h = 0
+    val fff = 0
 }
